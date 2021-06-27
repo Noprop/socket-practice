89283986598267cf5845616686c0a4a3aac1f956
Noprop <12greg21@gmail.com>
2021-06-26 22:20:36 -0400

    HTML response from google

--- google_socket.c
+++ google_socket.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
@@ -15,7 +16,8 @@ int main () {
   struct sockaddr_in server;
   server.sin_addr.s_addr = inet_addr("74.125.136.139");
   server.sin_family = AF_INET;
-  server.sin_port =  htons(80);
+  server.sin_port = htons(80);
+  // server.sin_port = htons(443);
 
   int connection_status = connect(network_socket, (struct sockaddr*) &server, sizeof(server));
 
@@ -24,7 +26,28 @@ int main () {
     return 0;
   }
 
-  printf("Connected to server");
+  printf("Connected to server\n");
+
+  char *message;
+  message = "GET / HTTP/1.1\r\n\r\n";
+
+  int send_status = send(network_socket, message, strlen(message), 0);
+
+  if (send_status == -1) {
+    printf("Send failed.");
+    return 0;
+  }
+
+  printf("Message sent.\n");
+
+  char server_reply[2000];
+  int receive_status = recv(network_socket, server_reply, 2000, 0);
+
+  if (receive_status == -1) {
+    printf("Receive failed.");
+  }
+
+  printf("Reply received: %s\n", server_reply);
 
   return 0;
 }
